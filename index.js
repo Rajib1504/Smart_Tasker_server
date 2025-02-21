5a19bd3387c11a5e03d78fed03248dd71a7f4524
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-02-21 22:43:44 +0530

    get route is done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
 
 //middleware
 app.use(express.json());
@@ -12,7 +12,7 @@ app.use(cors());
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://<${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tkglq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri =  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tkglq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,29 +26,43 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
-        //     await client.connect();
-      const Taskcollection = Tasks.db("smarttaskerDB").collection(Tasks);
+            await client.connect();
+      const Taskcollection = client.db("smarttaskerDB").collection("tasks");
 //    create 
-  app.post('/',async(req,res)=>{
+  app.post('/tasks',async(req,res)=>{
       const newtask=req.body 
       const result = await Taskcollection.insertOne(newtask)
       res.send(result)
+      console.log(result)
   })
 //   get 
-app.get('/',async(req,res)=>{
-      const tasks = await Taskcollection.find().toArray()
-      res.send(tasks)
+app.get('/tasks/:email',async(req,res)=>{
+      const {email} = req.params
+      try{
+            const tasks = await Taskcollection.find({email}).toArray()
+           
+            const todo = tasks.filter(task => task.category === "To-Do");
+            const inprogress = tasks.filter(task => task.category === "In Progress");
+            const done = tasks.filter(task => task.category === "Done");
+            res.json({todo ,inprogress,done})
+            console.log({todo,inprogress,done})
+      }
+      catch(error){
+            console.error('Fail to fetch tasks:', error); 
+            res.status(500).json({ message: 'Failed to fetch tasks' });
+      }
 })
 // update task  
-app.put('/task/:id',async(req,res)=>{
+app.put('/tasks/:id',async(req,res)=>{
       const {id}=req.params;
+      const updateData = req.body
       const result = await Taskcollection.updateOne(
             {_id:new ObjectId(id)},
-      {$set :updateDate})
+      {$set :updateData})
       res.send(result)
 })
 // delete 
-app.delete('task/:id',async(req,res)=>{
+app.delete('/tasks/:id',async(req,res)=>{
       const {id }=req.params
       const result = await Taskcollection.deleteOne({_id:new ObjectId(id)})
       res.send(result)
